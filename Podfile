5bd3d1815780739f115d4c7c62e893340955e05c
ericncku <erichoi0727@gmail.com>
2017-02-24 14:43:56 +0800

    facebook sign in teset completed

--- Podfile
+++ Podfile
@@ -10,5 +10,8 @@ target 'socialNetwork' do
   pod 'Firebase/Auth'
   pod 'Firebase/Database'
   pod 'Firebase/Storage'
+  pod 'FBSDKCoreKit'
+  pod 'FBSDKShareKit'
+  pod 'FBSDKLoginKit'
 
 end
--- socialNetwork/SigninVC.swift
+++ socialNetwork/SigninVC.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import FirebaseAuth
+
 
 class SigninVC: UIViewController {
 
@@ -20,6 +24,34 @@ class SigninVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func fbBtnPressed(_ sender: RoundButton) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("User cancelled Facebook authentication")
+            } else {
+                print("Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+        
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("Successfully authenticated with Firebase")
+                
+            }
+        })
+    }
 
 }
 
